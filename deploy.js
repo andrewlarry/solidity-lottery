4ed7c9b3831c8fb814927f0f5903315a83fa4d71
Andrew <afuselier23@gmail.com>
2018-07-30 09:37:23 -0700

    removing console.log statements

--- deploy.js
+++ deploy.js
@@ -9,11 +9,8 @@ const provider = new HDWalletProvider(mnemonic, TEST_ENDPOINT);
 
 const web3 = new Web3(provider);
 
-console.log(provider);
-
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('account: ', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: `0x${bytecode}` })
